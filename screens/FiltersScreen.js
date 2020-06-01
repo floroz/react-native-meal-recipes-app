cdecefbb000ebfd38013a1b655e87a1f961c4180
Daniele Tortora <daniele.tortora@secretescapes.com>
2020-06-01 09:46:48 +0100

    passing data through navigation params

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,12 +1,64 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Switch, Platform } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../components/HeaderButton";
+import colors from "../constants/colors";
 
-const FiltersScreen = () => {
+const FilterSwitch = (props) => {
+  return (
+    <View style={styles.filterContainer}>
+      <Text>{props.label}</Text>
+      <Switch
+        value={props.value}
+        onValueChange={(newValue) => props.onValueChange(newValue)}
+        trackColor={{ true: colors.primaryColor }}
+        thumbColor={Platform.OS === "android" && colors.primaryColor}
+      />
+    </View>
+  );
+};
+
+const FiltersScreen = ({ navigation }) => {
+  const [isGlutenFree, setIsGlutenFree] = React.useState(false);
+  const [isLactoseFree, setIsLactoseFree] = React.useState(false);
+  const [isVegeterian, setIsVegeterian] = React.useState(false);
+  const [isVegan, setIsVegan] = React.useState(false);
+
+  const saveFilters = React.useCallback(() => {
+    const appliedFilters = {
+      glutenFree: isGlutenFree,
+      lactoseFree: isLactoseFree,
+      vegeterian: isVegeterian,
+      vegan: isVegan,
+    };
+    console.log(appliedFilters);
+  }, [isGlutenFree, isLactoseFree, isVegan, isVegeterian]);
+
+  React.useEffect(() => {
+    navigation.setParams({
+      saveFilters,
+    });
+  }, [saveFilters]);
+
   return (
     <View style={styles.screen}>
-      <Text></Text>
+      <Text style={styles.title}>Available Filters</Text>
+      <FilterSwitch
+        label="Gluten Free"
+        value={isGlutenFree}
+        onValueChange={setIsGlutenFree}
+      />
+      <FilterSwitch
+        label="Lactose Free"
+        value={isLactoseFree}
+        onValueChange={setIsLactoseFree}
+      />
+      <FilterSwitch
+        label="Vegeterian"
+        value={isVegeterian}
+        onValueChange={setIsVegeterian}
+      />
+      <FilterSwitch label="Vegan" value={isVegan} onValueChange={setIsVegan} />
     </View>
   );
 };
@@ -18,6 +70,8 @@ FiltersScreen.navigationOptions = (navOptions) => {
 };
 
 FiltersScreen.navigationOptions = ({ navigation }) => {
+  const saveFilters = navigation.getParam("saveFilters");
+
   return {
     title: "Filters",
     headerLeft: () => (
@@ -29,6 +83,11 @@ FiltersScreen.navigationOptions = ({ navigation }) => {
         />
       </HeaderButtons>
     ),
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item title="Menu" iconName="ios-save" onPress={() => saveFilters()} />
+      </HeaderButtons>
+    ),
   };
 };
 
@@ -37,7 +96,19 @@ export default FiltersScreen;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "center",
-    alignContent: "center",
+    alignItems: "center",
+  },
+  filterContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "80%",
+    marginVertical: 15,
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    margin: 20,
+    textAlign: "center",
   },
 });
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,14 +1,35 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView, Image } from "react-native";
 import HeaderButton from "../components/HeaderButton";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
+const ListItem = (props) => {
+  return (
+    <View style={styles.listItem} {...props}>
+      <Text>{props.children}</Text>
+    </View>
+  );
+};
 
 const MealDetailScreen = ({ navigation: { getParam } }) => {
   const meal = getParam("meal");
   return (
-    <View>
-      <Text>{meal.title}</Text>
-    </View>
+    <ScrollView>
+      <Image source={{ uri: meal.imageUrl }} style={styles.image} />
+      <View style={styles.details}>
+        <Text>{meal.duration}m</Text>
+        <Text>{meal.complexity.toUpperCase()}</Text>
+        <Text>{meal.affordability.toUpperCase()}</Text>
+      </View>
+      <Text style={styles.title}>Ingredients</Text>
+      {meal.ingredients.map((ing) => (
+        <ListItem key={ing}>{ing}</ListItem>
+      ))}
+      <Text style={styles.title}>Steps</Text>
+      {meal.steps.map((step) => (
+        <ListItem key={step}>{step}</ListItem>
+      ))}
+    </ScrollView>
   );
 };
 
@@ -30,4 +51,26 @@ MealDetailScreen.navigationOptions = (navigationOptions) => {
 
 export default MealDetailScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 200,
+  },
+  details: {
+    flexDirection: "row",
+    padding: 15,
+    justifyContent: "space-around",
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    textAlign: "center",
+  },
+  steps: {},
+  listItem: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderColor: "#ccc",
+    borderWidth: 1,
+  },
+});
